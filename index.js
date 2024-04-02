616abca75751b854c3a50662330698260b4ce903
kiruthikadev-r <priyakiruthi21@gmail.com>
2024-04-02 15:31:54 +0530

    added welcome msg

--- index.js
+++ index.js
@@ -79,3 +79,7 @@ app.post('/login', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.post('/',async(req,res)=>{
+  res.send("vanakkam from JK")
+})
